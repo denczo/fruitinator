dea6668cc3fc5163f8c778ae1acf4d461a30132a
denczo <d.czogalla@gmx.de>
2023-05-16 00:02:19 +1000

    FIX: event bubbling

--- frontend/src/navigation/buttons/add/Add.js
+++ frontend/src/navigation/buttons/add/Add.js
@@ -3,10 +3,8 @@ import './Add.css';
 import store from "../../../stores/Store"
 // import { observer } from "mobx-react";
 
-
-
 const Add = (name, amount) => {
-    return (<div className="Add" onClick={() => store.addItemToCart(name, amount)}>
+    return (<div className="Add" onClick={(e) => {e.stopPropagation(); store.addItemToCart(name, amount)}}>
         {"+"}
         {/* <p>{"+"}</p> */}
     </div>);
