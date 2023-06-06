a6e93ef6641f3cb747bafea596b37294b50e4ad1
denczo <d.czogalla@gmx.de>
2023-06-06 17:06:17 +1000

    EDIT: adjusted styling

--- frontend/src/navigation/navbar/Navbar.js
+++ frontend/src/navigation/navbar/Navbar.js
@@ -4,10 +4,12 @@ import Searchbar from '../searchbar/Searchbar';
 import Back from '../buttons/back/Back';
 import Cart from '../buttons/cart/Cart';
 import Favorites from '../buttons/favorites/Favorites';
+import Logo from '../../structure/logo/Logo';
 
 
 const Navbar = () => {
     return (<div className='Navbar'>
+        {/* <Logo scale={80}/> */}
         <Back/>
         <Searchbar/>
         <Cart/>
