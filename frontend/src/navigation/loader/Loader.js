8464a95ca5ea91e98494cc44a80df91a3735b7f9
denczo <d.czogalla@gmx.de>
2024-02-26 09:54:59 +0100

    ADD: loading animation and async image loading

---
+++ frontend/src/navigation/loader/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Loader.sass';
+
+const Loader = () => {
+    return (<div className="loader"></div>);
+}
+
+export default Loader;
--- frontend/src/screens/productOverview/components/product/Product.js
+++ frontend/src/screens/productOverview/components/product/Product.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import store from "../../../../stores/Store"
 import './Product.sass';
@@ -6,10 +6,12 @@ import AddCorner from '../../../../navigation/buttons/add/addcorner/AddCorner';
 import Favorites from '../../../../navigation/buttons/favorites/Favorites';
 
 import { motion } from 'framer-motion';
+import Loader from '../../../../navigation/loader/Loader';
 
 const Product = ({ data }) => {
 
-        const { fruit, price, image } = data;
+    const { fruit, price, image } = data;
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate()
     const handleClick = (data) => {
         navigate('/info')
@@ -24,23 +26,25 @@ const Product = ({ data }) => {
         transition={{ duration: 0.2 }}
         layout={'position'}
         key={fruit}
-        >
-            <motion.div
-                className="Product"
-                whileHover={{ scale: 1.05 }}
-                layout                    
-                onClick={() => handleClick(data)}>
-                <div id='FavoritesPlacement'>
-                    <Favorites />
-                </div>
-                <div className='ProductContent'>
-                    <div><b>{fruit}</b></div>
-                    <div style={{fontSize: 18}}><b>{price}</b></div>
-                    <img key={fruit} src={image} loading="lazy" alt=""></img>
-                </div>
-                <AddCorner data={{ fruit, price, image }} />
-            </motion.div>
-        </motion.div>);
+    >
+        <motion.div
+            className="Product"
+            whileHover={{ scale: 1.05 }}
+            // layout
+            onClick={() => handleClick(data)}>
+                
+            <div id='FavoritesPlacement'>
+                <Favorites />
+            </div>
+            <div className='ProductContent'>
+                <div><b>{fruit}</b></div>
+                <div style={{ fontSize: 18 }}><b>{price}</b></div>
+                {loading? <Loader /> : <></>}
+                <img key={fruit} src={image} onLoad={() => setLoading(false)} loading="lazy" alt=""></img>
+            </div>
+            <AddCorner data={{ fruit, price, image }} />
+        </motion.div>
+    </motion.div>);
 }
 
 export default Product;
